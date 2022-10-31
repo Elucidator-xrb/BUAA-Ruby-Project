eddd690aa2574e7932e544e265dfcc69c29c1d40
Elucidator-xrb <1206587220@qq.com>
2022-10-31 10:10:02 +0800

    add welcome index & route distribute

--- cookbook3/config/routes.rb
+++ cookbook3/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'welcome/index' => 'welcome#index' 
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'welcome#index'
 end
