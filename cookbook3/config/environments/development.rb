fc202d3fc5e85c5e9c17dc5c59871516b48918b8
Elucidator-xrb <1206587220@qq.com>
2022-12-27 18:14:59 +0800

    stash, it has been a long time before the last edits

--- cookbook3/config/environments/development.rb
+++ cookbook3/config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts.clear
+  # config.hosts << "09353.cookbook3.rubyapp.act.buaa.edu.cn"
+
 end
