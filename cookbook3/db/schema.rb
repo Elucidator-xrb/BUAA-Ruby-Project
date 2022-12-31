4578cd803feeee5e2cd4346507c25057d9a49c09
Elucidator-xrb <1206587220@qq.com>
2022-12-31 22:23:18 +0800

    after adding the column instruction to recipes table

--- cookbook3/db/schema.rb
+++ cookbook3/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_023315) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_31_135757) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_31_023315) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id", null: false
+    t.string "instructions"
     t.index ["category_id"], name: "index_recipes_on_category_id"
   end
 
