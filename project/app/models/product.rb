f9216351df2b2e333e96523cca1b98af10207121
Elucidator-xrb <1206587220@qq.com>
2023-01-05 12:23:43 +0800

    add validates to product: uniqueness

--- project/app/models/product.rb
+++ project/app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   validates :pname, :description, :price, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0}
+  validates :pname, uniqueness: {case_sensitive: true}
   has_many :items, dependent: :destroy
 end
